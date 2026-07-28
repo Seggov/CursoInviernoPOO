cbe16c1c2a44c48e074171afb3244d947e707069
pedro <pedro.segovia@alumnos.ucn.cl>
2026-07-28 12:14:32 -0400

    Ultimo PUll

--- src/Main/ManejoErrores.java
+++ src/Main/ManejoErrores.java
@@ -1,11 +1,11 @@
 package Main;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class ManejoErrores {
 
 	private static Scanner SC = new Scanner(System.in);
-	
 	
 	public static void main(String[] args) {
 		ManejoErrores programa = new ManejoErrores();
@@ -16,15 +16,32 @@ public class ManejoErrores {
 	private void Iniciar() {
 		
 		try {
-			entradaUsuario();
+			lecturaArchivos("data.txt");
+			String name = entradaUsuario();
+			verificarSiesta(name);
 		} catch (Exception e) {
 			System.out.println("ERORR 404");
 		}
 	}
 
-	private void entradaUsuario() {
-		System.out.println("Ingresa tu nombre: "); String usuario = SC.nextLine();
+	private void lecturaArchivos(String string) {
+		File FILE = new File(string);
+		
+		String linea = SC.nextLine();
+		while (SC.hasNextLine()) {
+			
+		}
+		
+	}
 
+	private void verificarSiesta(String name) {
+		
+	}
+
+	private String entradaUsuario() {
+		System.out.println("Ingresa el nombre que quieres buscar: "); String usuario = SC.nextLine();
+		return usuario;
+		
 	}
 
 }
