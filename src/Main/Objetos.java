585e7bb601b259504025d9e0e0db5769dd741f5c
pedro <pedro.segovia@alumnos.ucn.cl>
2026-07-29 11:05:32 -0400

    Cambioobjetos

---
+++ src/Main/Objetos.java
@@ -0,0 +1,19 @@
+package Main;
+
+public class Objetos {
+	
+	
+	public static void main(String[] args) {
+		Objetos programa = new Objetos();
+		programa.iniciar();
+	}
+
+	private void iniciar() {
+		
+		int[] lista = new int[10];
+		lista = {1,2}; 
+		// TODO Auto-generated method stub
+		
+	}
+
+}
