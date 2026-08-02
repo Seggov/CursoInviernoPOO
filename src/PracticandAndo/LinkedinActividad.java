3e1b3eec9c15249ef6026f56fe448c36b1c565d4
Pedro Segovia <pedro.segovia@alumnos.ucn.cl>
2026-08-02 14:27:08 -0400

    Update LinkedinActividad.java

--- src/PracticandAndo/LinkedinActividad.java
+++ src/PracticandAndo/LinkedinActividad.java
@@ -1,8 +1,19 @@
 package PracticandAndo;
 
+import java.util.LinkedList;
+
 public class LinkedinActividad {
 
 	public static void main(String[] args) {
+		LinkedinActividad programa = new LinkedinActividad();
+		programa.iniciar();
+		
+	}
+
+	private void iniciar() {
+		
+		LinkedList<String> listaDinamica = new LinkedList<String>();
+		
 		
 	}
 
