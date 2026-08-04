2cf615ce1d4c57f4b1fac0a6bc5c136b43ba19e0
pedro <pedro.segovia@alumnos.ucn.cl>
2026-08-04 13:40:57 -0400

    Create App.java

---
+++ src/Ayudantia6/App.java
@@ -0,0 +1,14 @@
+package Ayudantia6;
+
+public class App {
+
+	public static void main(String[] args) {
+		App programa = new App();
+		programa.iniciar();
+	}
+
+	private void iniciar() {
+		
+	}
+
+}
